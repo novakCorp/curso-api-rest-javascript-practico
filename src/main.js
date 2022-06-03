3a70e0961e3d9dfb304a0769b6e7d3f0eec9e128
nova <nova.corps1010@outlook.com>
2022-06-03 15:31:24 -0400

    clase 4: Lista de peliculas en tendencia

--- src/main.js
+++ src/main.js
@@ -1,1 +1,27 @@
-console.log('Hello World from the main file!');
+async function getTrendingMoviesPreview() {
+    const res = await fetch('https://api.themoviedb.org/3/trending/movie/day?api_key=' + API_KEY);
+    const data = await res.json();
+
+    const movies = data.results;
+    console.log(movies);
+
+    movies.forEach((movie) => {
+
+        // Getting the child inside the section to render the movies from the api request
+        const trendingPreviewMoviesContainer = document.querySelector('#trendingPreview .trendingPreview-movieList');
+
+        const movieContainer = document.createElement('div');
+        movieContainer.classList.add('movie-container');
+
+        const movieImg = document.createElement('img');
+        movieImg.classList.add('movie-img');
+        movieImg.setAttribute('alt', movie.title);
+        movieImg.setAttribute('src', 'https://image.tmdb.org/t/p/w300' + movie.poster_path);
+
+        movieContainer.appendChild(movieImg);
+        trendingPreviewMoviesContainer.appendChild(movieContainer);
+    });
+
+}
+
+getTrendingMoviesPreview();
